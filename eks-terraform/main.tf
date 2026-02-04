7f816bfb6cb07fc31864f828805072c09ac2f025
Susan Amechi <86523724+Susan-hue@users.noreply.github.com>
2026-02-05 00:24:25 +0100

    Update IAM principal ARN for admin access

--- eks-terraform/main.tf
+++ eks-terraform/main.tf
@@ -143,7 +143,7 @@ module "eks" {
 
   access_entries = {
   admin = {
-    principal_arn = "arn:aws:iam::182025017187:user/Bigcephas"
+    principal_arn = "arn:aws:iam::567185414668:user/cloud-project"
 
     policy_associations = {
       admin = {
